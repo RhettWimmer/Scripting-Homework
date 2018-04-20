5c17112ed2d062e1a65893666ac755483be9603f
Unknown <thedovahbrine@gmail.com>
2018-04-19 19:59:06 -0600

    file

---
+++ Conventions and syntax.cs
@@ -0,0 +1,14 @@
+//In scripting, syntax is like grammar and Conventions is like a writer's style of writing
+//Syntax has to be written for the code to work. Written wrong, and the code will fail
+void Start(){
+    if(this){
+        do this;
+    }
+}
+//This code will work because the syntax is proper
+void start ()
+    IF(this)
+    do this
+    }
+}
+//This would not run because the syntax is wrong
